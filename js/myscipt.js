e001fd75a5d250a1aabcb5097a51ed886410658f
elisabichiappini <elisabichiappini@gmail.com>
2023-11-09 11:00:15 +0100

    feat: scaffolding first milestone

--- js/myscipt.js
+++ js/myscipt.js
@@ -4,6 +4,15 @@ FILE JS
 //codice restrittivo
 'use strict'
 
+// - (condizione) SE MULTIMO DI 3:
+// -- vero: stampo “Fizz” al posto del numero;
+// -- falso: lascio numero;
+// - (condizione) SE MULTIPLO DI 5:
+// -- vero: stampo “Buzz”;
+// -- falso: lascio numero;
+// - (condizione) S multipli di 3 E multiplo di 5;
+// -- vero: stampo “FizzBuzz”;
+// -- falso: lascio numero;
 
 // Programma:
 // - stampare in console i numeri da 1 a 100;
@@ -11,35 +20,22 @@ let valueString = '';
 // 1 BONUS 
 const gameUlList = document.querySelector('ul.numbers-generated');
 
-for(let gameNumber = 1; gameNumber < 101; gameNumber++) {
+for(let gameNumber = 1; gameNumber <= 100; gameNumber++) {
     const li = document.createElement ('li');
-    li.append (gameNumber + valueString);
-    gameUlList.append(li);
-
-    if (gameNumber % 3 === 0) {
+    
+    if (gameNumber % 3 === 0 && gameNumber % 5 === 0) {
+        valueString = 'FizzBuzz';
+        console.log(valueString)
+    } else if (gameNumber % 3 === 0) {
         valueString = 'Fizz';
-        console.log(gameNumber + valueString);
+        console.log(valueString);
     } else if (gameNumber % 5 === 0) {
         valueString = 'Buzz';
-        console.log(gameNumber + valueString);
+        console.log(valueString);
     } else {
-        valueString = '';
-        console.log(gameNumber + valueString);
+        valueString = gameNumber + '';
+        console.log(valueString);
     }
+    li.append (valueString);
+    gameUlList.append(li);
 };
-
-// - (condizione) SE MULTIMO DI 3:
-// -- vero: stampo “Fizz” al posto del numero;
-
-// -- falso: lascio numero;
-
-// - (condizione) SE MULTIPLO DI 5:
-// -- vero: stampo “Buzz”;
-
-// -- falso: lascio numero;
-
-// - (condizione) S multipli di 3 E multiplo di 5;
-// -- vero: stampo “FizzBuzz”;
-
-// -- falso: lascio numero;
-
